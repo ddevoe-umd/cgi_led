54161ba86d1b119ddd483445c89defa306a8b37e
ddevoe-umd <84353435+ddevoe-umd@users.noreply.github.com>
2021-10-07 12:33:17 -0400

    Update led-pwm_background.py

--- led-pwm_background.py
+++ led-pwm_background.py
@@ -15,7 +15,7 @@ pwm = GPIO.PWM(ledPin, 100) # PWM object on our pin at 100 Hz
 pwm.start(0) # start with LED off
 
 while True:
-  with open("led_pwm.txt", 'r') as file:
-    dutyCycle = float(file.read()) # read duty cycle value from file
+  with open("led_pwm.txt", 'r') as f:
+    dutyCycle = float(f.read()) # read duty cycle value from file
   pwm.ChangeDutyCycle(dutyCycle)
   time.sleep(0.1)
